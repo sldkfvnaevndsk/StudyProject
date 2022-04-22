a9a943c63bb677140f7c98cb840200b885a7a224
Oleg_Korovin <olegk.99@mail.ru>
2022-04-22 22:30:17 +0300

    create study project

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        int companyAmount = scan.nextInt();
+        int[][] information = new int[2][companyAmount];
+        for (int i = 0; i < information.length; i++) {
+            for (int j = 0; j < information[i].length; j++) {
+                information[i][j] = scan.nextInt();
+            }
+        }
+        int maxTax = -1; /*information[0][0] * information[1][0];*/
+        int company = -1;
+        for (int j = 0; j < information[0].length; j++) {
+            if (information[0][j] * information[1][j] > maxTax) {
+                maxTax = information[0][j] * information[1][j];
+                company = j;
+            }
+        }
+        System.out.print(company + 1);
+    }
+}
+
